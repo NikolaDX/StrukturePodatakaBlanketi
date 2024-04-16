b1640b5ec22b5e48495a960a6346b3a57d4639bc
NikolaDX <nikolar2903@gmail.com>
2024-04-16 11:08:27 +0200

    Oktobar 2023 zadatak 1

---
+++ 2023OktobarZad1/main.cpp
@@ -0,0 +1,20 @@
+#include "LList.h"
+
+int main() {
+	LList* l1 = new LList();
+
+	l1->addToHead(6);
+	l1->addToHead(5);
+	l1->addToHead(9);
+	l1->addToHead(1);
+	l1->addToHead(3);
+	l1->addToHead(4);
+
+	l1->printAll();
+
+	l1->rearrange(4, 5, 1);
+
+	l1->printAll();
+
+	return 0;
+}
